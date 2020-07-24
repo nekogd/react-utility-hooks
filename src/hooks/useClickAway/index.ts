749ba01b2a0abebdcba407243b048f2dfe3a6766
Tymek <tymoteusz.luczko@hyd.agency>
2020-07-24 21:22:28 +0200

    tests and renaming a bit

---
+++ src/hooks/useClickAway/index.ts
@@ -0,0 +1,34 @@
+import { useEffect, useCallback } from 'react';
+import { getTargetElement, BasicTarget } from '../../helpers';
+
+
+const defaultEvent = 'click';
+
+type EventType = MouseEvent | TouchEvent;
+
+export default function useClickAway(
+  onClickAway: (event: EventType) => void,
+  target: BasicTarget,
+  eventName: string = defaultEvent,
+) {
+  const handler = useCallback(
+    (event) => {
+      const targetElement = getTargetElement(target) as HTMLElement;
+
+      if (!targetElement || targetElement.contains(event.target)) {
+        return;
+      }
+
+      onClickAway(event);
+    },
+    [onClickAway, typeof target === 'function' ? undefined : target],
+  );
+
+  useEffect(() => {
+    document.addEventListener(eventName, handler);
+
+    return () => {
+      document.removeEventListener(eventName, handler);
+    };
+  }, [eventName, handler]);
+}
